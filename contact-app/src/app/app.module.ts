9fe9899364532b77f1c9f60449317c21032a856f
Tero <tero.pau@gmail.com>
2019-05-17 12:45:16 +0300

    wtf

--- contact-app/src/app/app.module.ts
+++ contact-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactListComponent
+    ContactListComponent,
+    ContactListItemComponent
   ],
   imports: [
     BrowserModule,

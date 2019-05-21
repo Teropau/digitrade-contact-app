855e01f5326bcb394185f36ae6e08c7150015ac3
Tero <tero.pau@gmail.com>
2019-05-21 10:07:21 +0300

    big update

--- contact-app/src/app/app.module.ts
+++ contact-app/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
 import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
+import {MatButtonModule} from '@angular/material';
+import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
-    ContactListItemComponent
+    ContactListItemComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

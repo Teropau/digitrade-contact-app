f088d49655d34010dc1334019c58cb26934ad43d
Tero <tero.pau@gmail.com>
2019-05-21 10:30:19 +0300

    interface added

--- contact-app/src/app/contact/contact.ts
+++ contact-app/src/app/contact/contact.ts
@@ -2,10 +2,21 @@ export class Contact {
   firstName: string;
   lastName: string;
   phone: string;
+  email: string;
+  address: IAddress;
 
-  constructor(phone?: string, firstName?: string, lastName?: string) {
+
+  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, address?: IAddress) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
+    this.email = email;
+    this.address = address;
   }
+}
+
+interface IAddress {
+  street: string;
+  postalCode: string;
+  city: string;
 }
